8cff59dd245e32e713fa94aea1443fa79f3f0267
Runji Wang <wangrunji0408@163.com>
2022-02-11 23:37:11 +0800

    multi thread
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,28 @@ use num_bigint::BigUint;
 
 const N: usize = 512;
 
-#[tokio::main(flavor = "current_thread")]
+#[tokio::main]
 async fn main() {
     let mut stream = reqwest::get("http://47.95.111.217:10001")
         .await
         .unwrap()
         .bytes_stream();
 
-    let m1: BigUint = "20220209192254".parse().unwrap();
-    let m2: BigUint = "104648257118348370704723099".parse().unwrap();
-    let m3: BigUint = "125000000000000064750000000000009507500000000000294357"
-        .parse()
-        .unwrap();
-    let zero: BigUint = "0".parse().unwrap();
+    // $ factor 20220209192254
+    // 20220209192254: 2 23 122509 3588061
+    // $ factor 104648257118348370704723099
+    // 104648257118348370704723099: 104648257118348370704723099
+    // $ factor 125000000000000064750000000000009507500000000000294357
+    // factor: ‘125000000000000064750000000000009507500000000000294357’ is too large
+    let m1: &_ = Box::leak(Box::new("20220209192254".parse::<BigUint>().unwrap()));
+    let m2: &_ = Box::leak(Box::new(
+        "104648257118348370704723099".parse::<BigUint>().unwrap(),
+    ));
+    let m3: &_ = Box::leak(Box::new(
+        "125000000000000064750000000000009507500000000000294357"
+            .parse::<BigUint>()
+            .unwrap(),
+    ));
 
     let mut s = vec![];
     while let Some(item) = stream.next().await {
@@ -24,22 +33,27 @@ async fn main() {
             while s.len() > N {
                 s.remove(0);
             }
-            for i in 0..s.len() {
-                if s[i] == b'0' {
-                    continue;
+            let s = s.clone();
+            tokio::spawn(async move {
+                let zero = "0".parse::<BigUint>().unwrap();
+                let even = s[s.len() - 1] % 2 == 0;
+                for i in 0..s.len() {
+                    if s[i] == b'0' {
+                        continue;
+                    }
+                    let n = BigUint::parse_bytes(&s[i..], 10).unwrap();
+                    if even && &n % m1 == zero {
+                        send(&s[i..]).await;
+                        println!("{}: {}", m1, n);
+                    } else if &n % m2 == zero {
+                        send(&s[i..]).await;
+                        println!("{}: {}", m2, n);
+                    } else if &n % m3 == zero {
+                        send(&s[i..]).await;
+                        println!("{}: {}", m3, n);
+                    }
                 }
-                let n = BigUint::parse_bytes(&s[i..], 10).unwrap();
-                if &n % &m1 == zero {
-                    println!("{}: {}", m1, n);
-                    send(&s[i..]).await;
-                } else if &n % &m2 == zero {
-                    println!("{}: {}", m2, n);
-                    send(&s[i..]).await;
-                } else if &n % &m3 == zero {
-                    println!("{}: {}", m3, n);
-                    send(&s[i..]).await;
-                }
-            }
+            });
         }
     }
 }
