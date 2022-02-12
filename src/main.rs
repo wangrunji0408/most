5ec68b5a91c2826b39569ec0519e841d52dfe700
Runji Wang <wangrunji0408@163.com>
2022-02-12 15:32:15 +0800

    N parallel
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use futures::future::try_join_all;
 use futures::stream::StreamExt;
 use num_bigint::BigUint;
 use std::collections::VecDeque;
-use std::sync::Arc;
 use std::time::Instant;
 
 const N: usize = 512;
@@ -30,75 +30,68 @@ async fn main() {
     ));
     let ms: [&_; 3] = [m1, m2, m3];
 
-    let mut deque = VecDeque::with_capacity(N);
-    // rem[k][i] = s[-i:] % m_k
-    let mut old = vec![vec![BigUint::new(vec![]); N + 1]; 3];
-    let mut new = vec![vec![BigUint::new(vec![]); N + 1]; 3];
+    let mut deque = VecDeque::new();
+    let mut rem: Vec<[BigUint; 3]> = vec![];
+    // rem[i][k] = deque[i..] % m[k]
     while let Some(item) = stream.next().await {
         let t0 = Instant::now();
-        for b in item.unwrap() {
-            while deque.len() >= N {
-                deque.pop_front();
-            }
-            deque.push_back(b);
-            let s = Arc::new(deque_to_vec(&deque));
-            let len = s.len();
+        let bytes = item.unwrap();
+        let tail_len = deque.len();
+        deque.extend(bytes.clone());
 
-            // update rem matrix
-            let x = b - b'0';
-            let step = 512 / 6;
-            let mut tasks = vec![];
-            for i0 in (1..=len).step_by(step) {
-                let s = s.clone();
-                let old: &'static Vec<Vec<BigUint>> = unsafe { std::mem::transmute(&old) };
-                let new: &'static mut Vec<Vec<BigUint>> = unsafe { std::mem::transmute(&mut new) };
-                tasks.push(tokio::spawn(async move {
-                    let zero = BigUint::default();
-                    for i in i0..(i0 + step).min(len + 1) {
-                        for k in 0..3 {
-                            new[k][i] = &old[k][i - 1] * 10u8 + x;
-                            while new[k][i] >= *ms[k] {
-                                new[k][i] -= ms[k];
-                            }
-                        }
-                        // test
-                        let n = &s[s.len() - i..];
-                        if n[0] == b'0' {
-                            continue;
-                        }
-                        if let Some(j) = new.iter().position(|r| r[i] == zero) {
-                            send(n).await;
-                            println!("{:?}", t0.elapsed());
-                            // println!(
-                            //     "{:?}: {}: {}",
-                            //     t0.elapsed(),
-                            //     ms[j],
-                            //     std::str::from_utf8(n).unwrap()
-                            // );
+        let mut tasks = vec![];
+        for i in 0..deque.len() {
+            let mut f = rem.get(i).cloned().unwrap_or_default();
+            let deque: &VecDeque<u8> = unsafe { std::mem::transmute(&deque) };
+            tasks.push(tokio::spawn(async move {
+                if deque[i] == b'0' {
+                    return Default::default();
+                }
+                let zero = BigUint::default();
+                for j in tail_len.max(i)..deque.len() {
+                    let len = j + 1 - i;
+                    if len > N {
+                        return f;
+                    }
+                    let x = deque[j] - b'0';
+                    for (f, m) in f.iter_mut().zip(ms) {
+                        *f = &*f * 10u8 + x;
+                        while &*f >= m {
+                            *f -= m;
                         }
                     }
-                }));
-            }
-            for t in tasks {
-                t.await.unwrap();
-            }
-            std::mem::swap(&mut old, &mut new);
+                    if let Some(k) = f.iter().position(|f| f == &zero) {
+                        let n: Vec<u8> = deque.range(i..=j).cloned().collect();
+                        send(n).await;
+                        println!("{:?}", t0.elapsed());
+                        // println!(
+                        //     "{:?}: {}: {}",
+                        //     t0.elapsed(),
+                        //     ms[k],
+                        //     std::str::from_utf8(&n).unwrap()
+                        // );
+                    }
+                }
+                f
+            }));
+        }
+        let mut new_rem = try_join_all(tasks).await.unwrap();
+        if deque.len() >= N {
+            rem = new_rem.split_off(deque.len() - (N - 1));
+        } else {
+            rem = new_rem;
+        }
+
+        while deque.len() >= N {
+            deque.pop_front();
         }
     }
 }
 
-fn deque_to_vec(d: &VecDeque<u8>) -> Vec<u8> {
-    let (s1, s2) = d.as_slices();
-    let mut s = Vec::with_capacity(d.len());
-    s.extend_from_slice(s1);
-    s.extend_from_slice(s2);
-    s
-}
-
-async fn send(body: &[u8]) {
+async fn send(body: Vec<u8>) {
     reqwest::Client::new()
         .post("http://47.95.111.217:10002/submit?user=omicron&passwd=y8J6IGKr")
-        .body(body.to_vec())
+        .body(body)
         .send()
         .await
         .unwrap();
