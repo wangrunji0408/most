5402ecc155fa74bb0c6cc60f1f4d97991e8f7a50
Runji Wang <wangrunji0408@163.com>
2022-02-14 01:40:58 +0800

    rewrite: simple M1
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::io::IoSlice;
 use std::time::{Duration, Instant};
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
+use tokio::sync::mpsc;
 
 const N: usize = 512;
 
@@ -27,32 +28,11 @@ async fn main() {
         .await
         .unwrap();
 
-    let (tx, mut rx) = tokio::sync::mpsc::channel::<(u8, Instant, Vec<u8>)>(8);
-    // responsor
+    let (tx, mut rx) = mpsc::channel::<TcpStream>(8);
     tokio::spawn(async move {
-        let t00 = Instant::now();
-        let mut dsum = Duration::default();
-        let mut count = 0;
         loop {
-            let mut stream = TcpStream::connect("47.95.111.217:10002").await.unwrap();
-            let (k, t0, body) = rx.recv().await.unwrap();
-
-            const HEADER: &str = "POST /submit?user=omicron&passwd=y8J6IGKr HTTP/1.1\r\nHost: 47.95.111.217:10002\r\nUser-Agent: Go-http-client/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\n";
-            let content_length = format!("Content-Length: {}\r\n\r\n", body.len());
-            let iov = [
-                IoSlice::new(HEADER.as_bytes()),
-                IoSlice::new(content_length.as_bytes()),
-                IoSlice::new(&body),
-            ];
-            stream.write_vectored(&iov).await.unwrap();
-
-            // statistics
-            let latency = t0.elapsed();
-            dsum += latency;
-            count += 1;
-            let avg = dsum / count;
-            let nps = count as f32 / t00.elapsed().as_secs_f32();
-            println!("M{k} lat: {latency:?}\tavg: {avg:?}\tnps: {nps:.3?}");
+            let stream = TcpStream::connect("47.95.111.217:10002").await.unwrap();
+            tx.send(stream).await.unwrap();
         }
     });
 
@@ -62,71 +42,79 @@ async fn main() {
     );
     let m3s = &*Vec::leak((0u8..10).map(|i| M3 * i).collect());
 
-    let mut deque = VecDeque::new();
-    let mut rem: Vec<(u64, u128, U256, U256)> = vec![];
-    // rem[i][k] = deque[i..] % m[k]
+    let mut stat = Stat::new();
+    let mut deque = VecDeque::with_capacity(N);
+    let mut rem: Vec<u64> = vec![0; N];
+    let mut valid: Vec<bool> = vec![false; N];
+    let mut pos = 0;
     while let Some(item) = body.data().await {
         let t0 = Instant::now();
         let bytes = item.unwrap();
-        let tail_len = deque.len();
-        deque.extend(bytes.clone());
 
-        let mut tasks = vec![];
-        for i in 0..deque.len() {
-            let mut f = match rem.get_mut(i) {
-                Some(f) => std::mem::take(f),
-                None => Default::default(),
-            };
-            let deque: &VecDeque<u8> = unsafe { std::mem::transmute(&deque) };
-            let tx = tx.clone();
-            tasks.push(tokio::spawn(async move {
-                if deque[i] == b'0' {
-                    return Default::default();
+        for b in bytes {
+            if deque.len() == N {
+                deque.pop_front();
+            }
+            deque.push_back(b);
+
+            rem[pos] = 0;
+            let x = b - b'0';
+            for f in &mut rem {
+                *f = (*f * 10 + x as u64) % M1;
+            }
+            valid[pos] = x != 0;
+            pos += 1;
+            if pos == N {
+                pos = 0;
+            }
+
+            for i in 0..deque.len() {
+                if valid[i] && rem[i] == 0 {
+                    let len = if i < pos { pos - i } else { N - (i - pos) };
+                    let n: Vec<u8> = deque.range(deque.len() - len..).cloned().collect();
+                    let tcp = rx.recv().await.unwrap();
+                    send(tcp, &n).await;
+                    stat.add(0, t0);
                 }
-                for j in tail_len.max(i)..deque.len() {
-                    let len = j + 1 - i;
-                    if len > N {
-                        return f;
-                    }
-                    let x = deque[j] - b'0';
-
-                    f.0 = (f.0 * 10 + x as u64) % M1;
-                    f.1 = rem_m2(f.1 * 10 + x as u128);
-                    // f.2 = (f.2 * 10u8 + x) % m3;
-                    f.2 = f.2 * 10 + x;
-                    let idx = m3s.partition_point(|m| &f.2 >= m);
-                    if idx > 0 {
-                        f.2 -= m3s[idx - 1];
-                    }
-                    const M4_MASK: U256 = U256([u64::MAX, u64::MAX, u64::MAX >> 14, 0]);
-                    f.3 = (f.3 * 10 + x) & M4_MASK;
-
-                    let k = match () {
-                        _ if f.0 == 0 => 1,
-                        _ if f.1 == 0 => 2,
-                        _ if f.2.is_zero() => 3,
-                        _ if f.3.is_zero() => 4,
-                        _ => 0,
-                    };
-                    if k != 0 {
-                        let n: Vec<u8> = deque.range(i..=j).cloned().collect();
-                        tx.send((k, t0, n)).await.unwrap();
-                        tokio::task::yield_now().await;
-                    }
-                }
-                f
-            }));
+            }
         }
-        let mut new_rem = try_join_all(tasks).await.unwrap();
-        if deque.len() >= N {
-            rem = new_rem.split_off(deque.len() - (N - 1));
-        } else {
-            rem = new_rem;
-        }
+    }
+}
 
-        while deque.len() >= N {
-            deque.pop_front();
+async fn send(mut tcp: TcpStream, body: &[u8]) {
+    const HEADER: &str = "POST /submit?user=omicron&passwd=y8J6IGKr HTTP/1.1\r\nHost: 47.95.111.217:10002\r\nUser-Agent: Go-http-client/1.1\r\nContent-Type: application/x-www-form-urlencoded\r\n";
+    let content_length = format!("Content-Length: {}\r\n\r\n", body.len());
+    let iov = [
+        IoSlice::new(HEADER.as_bytes()),
+        IoSlice::new(content_length.as_bytes()),
+        IoSlice::new(body),
+    ];
+    tcp.write_vectored(&iov).await.unwrap();
+}
+
+struct Stat {
+    t00: Instant,
+    dsum: Duration,
+    count: u32,
+}
+
+impl Stat {
+    fn new() -> Self {
+        Stat {
+            t00: Instant::now(),
+            dsum: Duration::default(),
+            count: 0,
         }
+    }
+
+    fn add(&mut self, k: u8, t0: Instant) {
+        // statistics
+        let latency = t0.elapsed();
+        self.dsum += latency;
+        self.count += 1;
+        let avg = self.dsum / self.count;
+        let nps = self.count as f32 / self.t00.elapsed().as_secs_f32();
+        println!("M{k} lat: {latency:?}\tavg: {avg:?}\tnps: {nps:.3?}");
     }
 }
 
