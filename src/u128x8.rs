ea831291d6ce66aa4461e363bb1664aef98d1e78
Runji Wang <wangrunji0408@163.com>
2022-02-20 01:42:00 +0800

    u128->32+64. perf regressed
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/u128x8.rs
+++ src/u128x8.rs
@@ -1,10 +1,10 @@
 use std::ops::{BitAnd, BitOr};
-use std::simd::{mask64x8, u64x8};
+use std::simd::{mask64x8, u64x8, u32x8};
 
 /// Vector of eight u128 values.
 #[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
 pub struct U128x8 {
-    hi: u64x8,
+    hi: u32x8,
     // low 60-bits used
     lo: u64x8,
 }
@@ -17,13 +17,13 @@ impl U128x8 {
 
     #[inline]
     pub fn is_zero(self) -> mask64x8 {
-        (self.hi | self.lo).lanes_eq(u64x8::splat(0))
+        (self.hi.cast() | self.lo).lanes_eq(u64x8::splat(0))
     }
 
     #[inline]
     pub const fn splat(x: u128) -> Self {
         Self {
-            hi: u64x8::splat((x >> 60) as u64),
+            hi: u32x8::splat((x >> 60) as u32),
             lo: u64x8::splat((x as u64) & MASK),
         }
     }
@@ -32,15 +32,15 @@ impl U128x8 {
     pub fn sub_on_ge(self, other: Self) -> Self {
         let mut c_lo = self.lo - other.lo;
         let mut c_hi = self.hi - other.hi;
-        let hi_eq = c_hi.lanes_eq(u64x8::default());
-        let hi_ge = (c_hi & u64x8::splat(1 << 63)).lanes_eq(u64x8::default());
+        let hi_eq = c_hi.lanes_eq(u32x8::default());
+        let hi_ge = (c_hi & u32x8::splat(1 << 31)).lanes_eq(u32x8::default());
         let lo_ge = (c_lo >> u64x8::splat(63)).lanes_eq(u64x8::default());
-        let ge = hi_eq.select_mask(lo_ge, hi_ge);
-        c_hi = c_hi - (c_lo >> u64x8::splat(63));
+        let ge = hi_eq.select_mask(lo_ge.into(), hi_ge);
+        c_hi = c_hi - (c_lo >> u64x8::splat(63)).cast();
         c_lo = c_lo & u64x8::splat(MASK);
         Self {
             hi: ge.select(c_hi, self.hi),
-            lo: ge.select(c_lo, self.lo),
+            lo: mask64x8::from(ge).select(c_lo, self.lo),
         }
     }
 
@@ -53,7 +53,7 @@ impl U128x8 {
     #[inline]
     pub fn mul10_add(self, b: u64) -> Self {
         let lo = self.lo * u64x8::splat(10) + u64x8::splat(b);
-        let hi = self.hi * u64x8::splat(10) + (lo >> u64x8::splat(60));
+        let hi = self.hi * u32x8::splat(10) + (lo >> u64x8::splat(60)).cast();
         Self {
             hi,
             lo: lo & u64x8::splat(MASK),
